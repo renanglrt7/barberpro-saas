331f8bce703b82912799dd9d3815156be872acaf
Renan Goulart <171586955+renanglrt7@users.noreply.github.com>
2026-07-06 20:25:27 -0300

    feat: criar estrutura inicial do projeto

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-white">
+        Bem-vindo ao BarberPro SaaS 💈
+      </h1>
+    </main>
+  );
+}
